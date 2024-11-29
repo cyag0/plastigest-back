423dd3cc168aed176f162f59e6b5fcf3a5ec1461
Gabito <pv210110238@vallarta.tecmm.edu.mx>
2024-11-29 15:16:55 -0600

    wip: usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,21 @@ class UserController extends BaseController
 
     protected function storeValidationRules(Request $request): array
     {
-        return $request->validate([]);
+        return $request->validate([
+
+            'name' => ['required','string','max:255'], 
+            'email' => ['required','string','email','max:255','unique:users,email,'.$request->route('user')],
+            'email_verfied_at' =>['required','string'],
+            'password' => ['required','string','min:8','confirmed'],
+            'last_name' => ['required','string'],
+            'phone_number' => ['required','string'],
+            'address' => ['required','string'],
+            'image'=> ['nullable'],
+            'is_active'=> ['boolean'],
+            'role_id' => ['required','numeric'],
+
+
+        ]);
     }
 
     protected function updateValidationRules(Request $request): array
